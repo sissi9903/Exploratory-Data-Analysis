df70ae821a2b05ce316400b0bf811cbd3f43cc71
sissi9903 <33793066+sissi9903@users.noreply.github.com>
2018-01-30 15:15:18 +0100

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+png( file = "plot1.png")
+
+z <- read.csv("Power.txt", sep = ";", stringsAsFactors = FALSE, na.strings = "?")
+z$Date <- as.Date(z$Date, "%d/%m/%Y")
+a <- as.Date("31/01/2007", "%d/%m/%Y")
+b <- as.Date("03/02/2007", "%d/%m/%Y")
+Power <- subset(z, Date > a & Date < b)
+
+par(mar = c(5,6,4,4))
+hist(Power$Global_active_power, col = "red", breaks = 16, xlab = "Gloabl Active Power (kilowatts)", main = "Global Active Power")
+
+dev.off()
+
